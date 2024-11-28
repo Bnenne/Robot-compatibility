457b0ac9e691f7d47a42b1d01969896f4abb033f
Benjamin Garcia <benjg208@gmail.com>
2024-11-28 09:08:07 -0500

    switched paths to /auto

--- app.py
+++ app.py
@@ -10,32 +10,32 @@ app = Flask(__name__)
 def get_scoutingapi():
     return jsonify(return_scoutingapi()), 200
 
-@app.route("/graph/<event>/<team>", methods=['GET'])
+@app.route("/auto/graph/<event>/<team>", methods=['GET'])
 @cross_origin()
 def get_graph(event, team):
     title = "/graph/" + event + "/" + team
     dl = DataLabeling(event, team)
     return send_file(dl.return_graph(title), mimetype='image/png'), 200
 
-@app.route("/data/<event>/<team>", methods=['GET'])
+@app.route("/auto/data/<event>/<team>", methods=['GET'])
 @cross_origin()
 def get_data(event, team):
     dl = DataLabeling(event, team)
     return jsonify(dl.return_data()), 200
 
-@app.route("/compare/graph/<event>/<team1>/<team2>", methods=['GET'])
+@app.route("/auto/compare/graph/<event>/<team1>/<team2>", methods=['GET'])
 @cross_origin()
 def get_compare_graph2(event, team1, team2):
     teams = [team1, team2]
-    title = "/compare/graph/"+event+"/"+team1+"/"+team2
+    title = "/auto/compare/graph/"+event+"/"+team1+"/"+team2
     compare = Compare(event, teams)
     return send_file(compare.return_compare_graph(title), mimetype='image/png'), 200
 
-@app.route("/compare/graph/<event>/<team1>/<team2>/<team3>", methods=['GET'])
+@app.route("/auto/compare/graph/<event>/<team1>/<team2>/<team3>", methods=['GET'])
 @cross_origin()
 def get_compare_graph3(event, team1, team2, team3):
     teams = [team1, team2, team3]
-    title = "/compare/graph/" + event + "/" + team1 + "/" + team2 +"/" + team3
+    title = "/auto/compare/graph/" + event + "/" + team1 + "/" + team2 +"/" + team3
     compare = Compare(event, teams)
     return send_file(compare.return_compare_graph(title), mimetype='image/png'), 200
   
